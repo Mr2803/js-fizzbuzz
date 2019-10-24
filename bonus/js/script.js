d9dca1f4940af55adcffb15ee9ca93406ed2ca85
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-24 18:07:23 +0200

    fix layout , add details

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -30,15 +30,15 @@ var j = 1;
 while( j<=100){
   //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz(devo inserirla prima perchè se la inserissi dopo js troverebbe gia prima la soluzione e quindi non arriverebbe a questa condizione)
   if(j % 3 == 0 && j % 5 == 0){
-    document.getElementById('stampaCicloWhile').innerHTML += "FizzBuzz" + " " ;
+    document.getElementById('stampaCicloWhile').innerHTML += "(🍺+🐝)" + " " ;
     console.log("FizzBuzz")
     //per i multipli di 3 stampi “Fizz” al posto del numero
   } else if(j % 3 == 0){
-    document.getElementById('stampaCicloWhile').innerHTML += "Fizz" + " ";
+    document.getElementById('stampaCicloWhile').innerHTML += "🍺" + " ";
     console.log("Fizz")
     // per i multipli di 5 stampi Buzz
   } else if(j % 5 == 0 ){
-    document.getElementById('stampaCicloWhile').innerHTML += "Buzz" + " ";
+    document.getElementById('stampaCicloWhile').innerHTML += "🐝" + " ";
     console.log("Buzz")
   } else{
     //devo inserirlo all'interno di un else altrimenti lo stamperebbe in console a prescindere e le scritte non sostituirebbero i numeri ma si porrebbero prima di loro
@@ -55,19 +55,19 @@ var k = 1;
 do{
   //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz(devo inserirla prima perchè se la inserissi dopo js troverebbe gia prima la soluzione e quindi non arriverebbe a questa condizione)
   if(k % 3 == 0 && k % 5 == 0){
-    document.getElementById('stampaCicloWhileDo').innerHTML +=  "FizzBuzz" + " ";
+    document.getElementById('stampaCicloDoWhile').innerHTML +=  "FizzBuzz" + " ";
     console.log("FizzBuzz")
     //per i multipli di 3 stampi “Fizz” al posto del numero
   } else if(k % 3 == 0){
-    document.getElementById('stampaCicloWhileDo').innerHTML +=  "Fizz" + " ";
+    document.getElementById('stampaCicloDoWhile').innerHTML +=  "Fizz" + " ";
     console.log("Fizz")
     // per i multipli di 5 stampi Buzz
   } else if(k % 5 == 0 ){
-    document.getElementById('stampaCicloWhileDo').innerHTML +=  "Buzz" + " ";
+    document.getElementById('stampaCicloDoWhile').innerHTML +=  "Buzz" + " ";
     console.log("Buzz")
   } else{
     //devo inserirlo all'interno di un else altrimenti lo stamperebbe in console a prescindere e le scritte non sostituirebbero i numeri ma si porrebbero prima di loro
-    document.getElementById('stampaCicloWhileDo').innerHTML += k + " ";
+    document.getElementById('stampaCicloDoWhile').innerHTML += k + " ";
       console.log(k)//debug
   }
 //VIA DI USCITA DAL CICLO
