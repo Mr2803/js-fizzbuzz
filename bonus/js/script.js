e185028ead4824dff32338350cd1db77a30bde95
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-24 20:36:45 +0200

    introduced function to print the solution to the click

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -5,49 +5,70 @@ e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 
 // NOTE: PROVA CON CICLO FOR
-for(i=1; i<=100; i++){
-  //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz(devo inserirla prima perchè se la inserissi dopo js troverebbe gia prima la soluzione e quindi non arriverebbe a questa condizione)
-  if(i % 3 == 0 && i % 5 == 0){
-    document.getElementById('stampaCicloFor').innerHTML +=  "<span style=color:#86FF8C;>" + "FizzBuzz"  + "</span>" + " " ;
-    console.log("FizzBuzz")
-    //per i multipli di 3 stampi “Fizz” al posto del numero
-  } else if(i % 3 == 0){
-    document.getElementById('stampaCicloFor').innerHTML +=  "<span style=color:#6495ED;>" + "Fizz"  + "</span>" + " " ;
-    console.log("Fizz")
-    // per i multipli di 5 stampi Buzz
-  } else if(i % 5 == 0 ){
-    document.getElementById('stampaCicloFor').innerHTML +=  "<span style=color:#FFB45E;>" + "Buzz"  + "</span>" + " " ;
-    console.log("Buzz")
-  } else{
-    //devo inserirlo all'interno di un else altrimenti lo stamperebbe in console a prescindere e le scritte non sostituirebbero i numeri ma si porrebbero prima di loro
-      document.getElementById('stampaCicloFor').innerHTML += "<span>" + i  + "</span>" +  " " ;
-      console.log(i)//debug
+for(var num=1; num<=100; num++){
+  document.getElementById('cicloForAfter').innerHTML += "<span>" + num  + "</span>" +  " " ;
+}
+
+
+
+
+
+function start(){
+  for(var i=1; i<=100; i++){
+    //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz(devo inserirla prima perchè se la inserissi dopo js troverebbe gia prima la soluzione e quindi non arriverebbe a questa condizione)
+    if(i % 3 == 0 && i % 5 == 0){
+      document.getElementById('stampaCicloFor').innerHTML +=  "<span style=color:#86FF8C;>" + "FizzBuzz"  + "</span>" + " " ;
+      console.log("FizzBuzz")
+      //per i multipli di 3 stampi “Fizz” al posto del numero
+    } else if(i % 3 == 0){
+      document.getElementById('stampaCicloFor').innerHTML +=  "<span style=color:#6495ED;>" + "Fizz"  + "</span>" + " " ;
+      console.log("Fizz")
+      // per i multipli di 5 stampi Buzz
+    } else if(i % 5 == 0 ){
+      document.getElementById('stampaCicloFor').innerHTML +=  "<span style=color:#FFB45E;>" + "Buzz"  + "</span>" + " " ;
+      console.log("Buzz")
+    } else{
+      //devo inserirlo all'interno di un else altrimenti lo stamperebbe in console a prescindere e le scritte non sostituirebbero i numeri ma si porrebbero prima di loro
+        document.getElementById('stampaCicloFor').innerHTML += "<span>" + i  + "</span>" +  " " ;
+        console.log(i)//debug
+    }
+    document.getElementById('cicloForAfter').style.display = "none";
   }
 }
 
 // NOTE: PROVA CON CICLO WHILE
-var j = 1;
-while( j<=100){
-  //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz(devo inserirla prima perchè se la inserissi dopo js troverebbe gia prima la soluzione e quindi non arriverebbe a questa condizione)
-  if(j % 3 == 0 && j % 5 == 0){
-    document.getElementById('stampaCicloWhile').innerHTML += "(🍺+🐝)" + " " ;
-    console.log("FizzBuzz")
-    //per i multipli di 3 stampi “Fizz” al posto del numero
-  } else if(j % 3 == 0){
-    document.getElementById('stampaCicloWhile').innerHTML += "🍺" + " ";
-    console.log("Fizz")
-    // per i multipli di 5 stampi Buzz
-  } else if(j % 5 == 0 ){
-    document.getElementById('stampaCicloWhile').innerHTML += "🐝" + " ";
-    console.log("Buzz")
-  } else{
-    //devo inserirlo all'interno di un else altrimenti lo stamperebbe in console a prescindere e le scritte non sostituirebbero i numeri ma si porrebbero prima di loro
-      document.getElementById('stampaCicloWhile').innerHTML += j + " ";
-      console.log(j)//debug
-  }
-//VIA DI USCITA DAL CICLO
-  j++;
+var num2 =1;
+while(num2<=100){
+  document.getElementById('cicloWhileAfter').innerHTML +=  num2 +" ";
+  num2++;
 }
+function start2(){
+  var j = 1;
+  while( j<=100){
+    //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz(devo inserirla prima perchè se la inserissi dopo js troverebbe gia prima la soluzione e quindi non arriverebbe a questa condizione)
+    if(j % 3 == 0 && j % 5 == 0){
+      document.getElementById('stampaCicloWhile').innerHTML += "(🍺+🐝)" + " " ;
+      console.log("FizzBuzz")
+      //per i multipli di 3 stampi “Fizz” al posto del numero
+    } else if(j % 3 == 0){
+      document.getElementById('stampaCicloWhile').innerHTML += "🍺" + " ";
+      console.log("Fizz")
+      // per i multipli di 5 stampi Buzz
+    } else if(j % 5 == 0 ){
+      document.getElementById('stampaCicloWhile').innerHTML += "🐝" + " ";
+      console.log("Buzz")
+    } else{
+      //devo inserirlo all'interno di un else altrimenti lo stamperebbe in console a prescindere e le scritte non sostituirebbero i numeri ma si porrebbero prima di loro
+        document.getElementById('stampaCicloWhile').innerHTML += j + " ";
+        console.log(j)//debug
+    }
+  //VIA DI USCITA DAL CICLO
+    j++;
+  }
+document.getElementById('cicloWhileAfter').style.display = "none";
+}
+
+
 
 // NOTE: PROVA CON CICLO WHILE DO
 
