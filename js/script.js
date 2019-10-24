0ba18bf9cad370d5a89de29f1143a371aa621cb4
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-24 13:42:48 +0200

    tested all 3 cycles. (How predictable it works for everyone)

--- js/script.js
+++ js/script.js
@@ -4,10 +4,9 @@ ma per i multipli di 3 stampi “Fizz” al posto del numero
 e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 
-
+// NOTE: PROVA CON CICLO FOR
 for(i=1; i<=100; i++){
-
- //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz(devo inserirla prima perchè se la inserissi dopo js troverebbe gia prima la soluzione e quindi non arriverebbe a questa condizione)
+  //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz(devo inserirla prima perchè se la inserissi dopo js troverebbe gia prima la soluzione e quindi non arriverebbe a questa condizione)
   if(i % 3 == 0 && i % 5 == 0){
     console.log("FizzBuzz")
     //per i multipli di 3 stampi “Fizz” al posto del numero
@@ -22,3 +21,47 @@ for(i=1; i<=100; i++){
   }
 
 }
+
+// NOTE: PROVA CON CICLO WHILE
+var j = 1;
+
+while( j<=100){
+  //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz(devo inserirla prima perchè se la inserissi dopo js troverebbe gia prima la soluzione e quindi non arriverebbe a questa condizione)
+  if(j % 3 == 0 && j % 5 == 0){
+    console.log("FizzBuzz")
+    //per i multipli di 3 stampi “Fizz” al posto del numero
+  } else if(j % 3 == 0){
+    console.log("Fizz")
+    // per i multipli di 5 stampi Buzz
+  } else if(j % 5 == 0 ){
+    console.log("Buzz")
+  } else{
+    //devo inserirlo all'interno di un else altrimenti lo stamperebbe in console a prescindere e le scritte non sostituirebbero i numeri ma si porrebbero prima di loro
+      console.log(j)//debug
+  }
+
+  j++;
+}
+
+// NOTE: PROVA CON CICLO WHILE DO
+
+var k = 1;
+
+do{
+  //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz(devo inserirla prima perchè se la inserissi dopo js troverebbe gia prima la soluzione e quindi non arriverebbe a questa condizione)
+  if(k % 3 == 0 && k % 5 == 0){
+    console.log("FizzBuzz")
+    //per i multipli di 3 stampi “Fizz” al posto del numero
+  } else if(k % 3 == 0){
+    console.log("Fizz")
+    // per i multipli di 5 stampi Buzz
+  } else if(k % 5 == 0 ){
+    console.log("Buzz")
+  } else{
+    //devo inserirlo all'interno di un else altrimenti lo stamperebbe in console a prescindere e le scritte non sostituirebbero i numeri ma si porrebbero prima di loro
+      console.log(k)//debug
+  }
+
+  k++;
+
+}while(k<=100)
